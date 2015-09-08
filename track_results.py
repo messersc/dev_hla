5eebdab90039f0a0a5769cde3294102402280939
Clemens Messerschmidt <cmessers@bcu6>
2015-09-08 16:36:05 +0200

    new repo for things unrelated to the database and folder structure

---
+++ track_results.py
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Clemens Messerschmidt
+# collect the results of different tools for hla typing into a coherent table/db for further processing etc
+
+# INPUT: directory of analysis results, multiple results per sample in dirs with controlled naming
+# OUTPUT: table summarizing our findings
+
+def findresults(dir):
+
