bb92f973c689097e03328b2505c8ca355d8ded37
Clemens Messerschmidt <cmessers@deb7-64eng.charite.de>
2015-09-08 09:28:49 +0200

    provide the wrapper with all 20 cell lines

--- hlatype_bwakit.sh
+++ hlatype_bwakit.sh
@@ -31,7 +31,7 @@ export -f run_bwakit
 
 
 
-for sample in CELL_ID_16; 
+for sample in CELL_ID_108 CELL_ID_109 CELL_ID_122 CELL_ID_13 CELL_ID_16 CELL_ID_163 CELL_ID_165 CELL_ID_18 CELL_ID_21 CELL_ID_235 CELL_ID_36 CELL_ID_38 CELL_ID_39 CELL_ID_41 CELL_ID_45 CELL_ID_56 CELL_ID_6 CELL_ID_79 CELL_ID_94 CELL_ID_99; 
 do
 	mkdir -p ../analysis/${sample}/hla_typing/bwakit/
 	run_bwakit ${sample} | sh
