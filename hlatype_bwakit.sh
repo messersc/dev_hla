89f526ab062a6e624266e6b26978393614c653c4
Clemens Messerschmidt <cmessers@bcu6>
2015-09-09 10:52:28 +0200

    remove cruft from hlatype_bwakit

--- hlatype_bwakit.sh
+++ hlatype_bwakit.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 module load bwakit
 
-repeat_arg()
-{
-    arg=$1
-    shift
-    set -x
-
-    for x in $*; do
-        echo $arg $x
-    done
-}
-
 run_bwakit()
 {
     sample=$1
@@ -23,12 +12,7 @@ run_bwakit()
         ../samples/${sample}/fastq/original/*_R1_*.fastq.gz \
         ../samples/${sample}/fastq/original/*_R2_*.fastq.gz
 }
-export -f repeat_arg
 export -f run_bwakit
-
-
-# we can copy over the log files later if we like
-
 
 
 for sample in CELL_ID_108 CELL_ID_109 CELL_ID_122 CELL_ID_13 CELL_ID_16 CELL_ID_163 CELL_ID_165 CELL_ID_18 CELL_ID_21 CELL_ID_235 CELL_ID_36 CELL_ID_38 CELL_ID_39 CELL_ID_41 CELL_ID_45 CELL_ID_56 CELL_ID_6 CELL_ID_79 CELL_ID_94 CELL_ID_99; 
@@ -36,6 +20,3 @@ do
 	mkdir -p ../analysis/${sample}/hla_typing/bwakit/
 	run_bwakit ${sample} | sh
 done
-
-
-#parallel -j 2 -v 'run_optitype {}' ::: BIH-002 BIH-004 BIH-016 BIH-02
