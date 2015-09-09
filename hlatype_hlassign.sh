4d78ccb7c558cb42840bd923ccddf2f9b0d9a2dc
Clemens Messerschmidt <cmessers@bcu6>
2015-09-09 15:44:07 +0200

    working draft for running serially on all pilot cell lines

--- hlatype_hlassign.sh
+++ hlatype_hlassign.sh
@@ -7,11 +7,11 @@ export PDIR=/home/cmessers/biotools/HLAssign_cli_pilot/pilot_source_code
 # load modules
 # module load BLAT
 export PATH=${PATH}:/home/cmessers/biotools/BLAT/
-module load R
+module load R || echo No R module
 
 run_hlassign()
 {
-    sample=$1
+    export sample=$1
     set -x
 		${PDIR}/run_HLAssign_CLI \
 		../samples/${sample}/fastq/original/*_R1_*.fastq.gz \
@@ -21,7 +21,7 @@ run_hlassign()
 export -f run_hlassign
 
 
-for sample in CELL_ID_108 # CELL_ID_109 CELL_ID_122 CELL_ID_13 CELL_ID_16 CELL_ID_163 CELL_ID_165 CELL_ID_18 CELL_ID_21 CELL_ID_235 CELL_ID_36 CELL_ID_38 CELL_ID_39 CELL_ID_41 CELL_ID_45 CELL_ID_56 CELL_ID_6 CELL_ID_79 CELL_ID_94 CELL_ID_99; 
+for sample in CELL_ID_108 CELL_ID_109 CELL_ID_122 CELL_ID_13 CELL_ID_16 CELL_ID_163 CELL_ID_165 CELL_ID_18 CELL_ID_21 CELL_ID_235 CELL_ID_36 CELL_ID_38 CELL_ID_39 CELL_ID_41 CELL_ID_45 CELL_ID_56 CELL_ID_6 CELL_ID_79 CELL_ID_94 CELL_ID_99; 
 do
 	mkdir -p ../analysis/${sample}/hla_typing/hlassign/
 	run_hlassign ${sample}
