d8402f6b377ee082d24fdf93b8a15d40310ed9c5
Clemens Messerschmidt <cmessers@bcu6>
2015-09-14 10:43:48 +0200

    R script for pulling together cleaned up results and further processing

--- accordance.R
+++ accordance.R
@@ -1,5 +1,5 @@
-# load all result files
-# samples should be ordered. 
+options(stringsAsFactors = FALSE)
+library(stringr)
 
 # # To generate the ref file, something like
 # ref = nar.02838.met.k.2014.File005[3:nrow(nar.02838.met.k.2014.File005), grep(pattern = ".*reference", nar.02838.met.k.2014.File005[2, ])]
@@ -7,43 +7,74 @@
 # rownames(ref) =  nar.02838.met.k.2014.File005[3:nrow(nar.02838.met.k.2014.File005), 1]
 # make.names(nar.02838.met.k.2014.File005[2, grep(pattern = ".*reference", nar.02838.met.k.2014.File005[2, ])], unique = T)
 # colnames(ref) = make.names(nar.02838.met.k.2014.File005[2, grep(pattern = ".*reference", nar.02838.met.k.2014.File005[2, ])], unique = T)
+# # was used. 
 
+# load all result files
+# samples should be ordered. 
+load_results <- function(d = getwd()){
+  ref <<- read.table("ref.csv", header=TRUE, quote="\"")
+  bwakit <<- read.table("bwakit", quote="\"", row.names=1)[1:6]
+  hlassign <<- read.table("hlassign", quote="\"", row.names=1)
+  optitype <<- read.table("optitype", quote="\"", row.names=1)
+}
 
-ref <- read.table("/vol/cs02/scratch/cmessers/projects/BIH/HLA/ref.csv", header=TRUE, quote="\"")
-bwakit <- read.table("/vol/cs02/scratch/cmessers/projects/BIH/HLA/bwakit", quote="\"")
-hlassign <- read.table("/vol/cs02/scratch/cmessers/projects/BIH/HLA/hlassign", quote="\"")
-optitype <- read.table("/vol/cs02/scratch/cmessers/projects/BIH/HLA/optitype", quote="\"")
-
-typer = c("optitype", 'bwakit', "hlassign")
-
-#get consensus from ref
-samples=bwakit[,1]
-
-# sort acc. to results
-xref = ref[match(samples, rownames(ref)), 1:6]
-
-acc = matrix(0, nrow=length(samples), ncol = 4)
-rownames(acc) = samples
-colnames(acc) = c(typer, "possible 4digit hits")
-
-for (refrow in 1:nrow(xref)){
-# for (refrow in 6){
-  hlatype = c()
-  # for(refcol in 1:ncol(xref)){
-  for(refcol in 1:6){
-    hlatype = append(hlatype, gsub(pattern = "(^[ABC]\\*[0-9]{2}:[0-9]{2,3}).*", replacement = '\\1', xref[refrow, refcol]))
-    
-    if (nchar(xref[refrow,refcol]) >= 7){
-      acc[refrow, 4] = acc[refrow, 4] + 1
-    }
-    
-    for (method in 1:length(typer)){          
-      hlaprediction = c()
-      for(refcol in 1:6){
-        hlaprediction = append(hlaprediction, gsub(pattern = "(^[ABC]\\*[0-9]{2}:[0-9]{2,3}).*", replacement = '\\1', get(typer[method])[refrow, refcol+1]))
-        # print(c(hlaprediction, hlatype, sum(hlaprediction %in% hlatype)))
-        acc[refrow, method] = sum(hlaprediction %in% hlatype)
-      }
-    }      
+fit_allele_to_precision <- function(str, regex, ncolon){
+  if (str == "0") {
+    return("nottyped")
+  } else if (sum(str_count(str, pattern=":")) >= ncolon) {
+   return(gsub(regex, replacement = '\\1', str))
+  } else {
+    return("nottyped")
   }
 }
+
+compare_allele_pairs <- function(x,y){
+  s = sum(x %in% y)
+  # test for homozygosity
+  if (x[1] == x[2]) if (s == 2) s = s - length(setdiff(y,x))
+  return(s)
+}
+
+build_performance_table <- function(typer = c("optitype", "bwakit", "hlassign"), precision="4d"){
+  
+  if (precision == "2d"){ pattern = "(^[ABC]\\*[0-9]{2}).*" ; ncolon = 0}
+  else if (precision == "4d"){ pattern = "(^[ABC]\\*[0-9]{2}:[0-9]{2,3}).*" ; ncolon = 1}
+  else if (precision == "8d"){ pattern = "(.*).*" ; ncolon = 3}
+  else return('STOP')
+  
+  #get consensus/sample names/IDs
+  samples=row.names(bwakit)
+  # sort and filter reference according to results
+  xref <<- ref[match(samples, rownames(ref)), 1:6]
+  
+  for (x in c("xref", typer)){
+    assign(x, apply(get(x), c(1,2), fit_allele_to_precision, pattern, ncolon)) #cellwise trimming of type to wanted precision
+  }
+  
+  # for manual inspection and comparisons
+  all_alleles = xref
+  for (x in typer){
+    all_alleles = cbind(all_alleles, get(typer))
+  }
+  
+  #build matrix
+  acc = matrix(0, nrow=length(samples), ncol = length(typer)+1)
+  rownames(acc) = samples
+  colnames(acc) = c(typer, "possible hits")
+  acc[, ncol(acc)] = apply(xref, 1, function(x) sum(x != "nottyped"))
+  
+  typercount = 0
+  for (x in typer){
+    hits = rep(0, length(samples))
+    for (a in c(1,3,5)){ # diploid organisms are fun...
+      pred = get(x)
+      hits = hits + sapply(1:nrow(xref), function(i) compare_allele_pairs(pred[i,a:(a+1)], xref[i,a:(a+1)]))
+    }
+    typercount = typercount + 1
+    acc[ ,typercount] = hits
+  }
+  return(list("accordance" = acc, "all_alleles" = all_alleles))
+}
+
+load_results()
+acc = build_performance_table(prec='2d')
