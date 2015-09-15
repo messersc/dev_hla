cdb435a38e7d35febfed34a7c6ce440bc2c93596
Clemens Messerschmidt <cmessers@bcu6>
2015-09-15 14:32:21 +0200

    fixed colnames bug that mixed up table. added mode function and consensus column

--- reshape_table.R
+++ reshape_table.R
@@ -13,21 +13,26 @@ reshape_results <- function(results_table = acc[[2]]) {
     }
   }
   
-  colnames(reshaped) = reshaped[1,]
+  colnames(reshaped) = make.names(reshaped[1,], unique = T)
   reshaped = tail(reshaped, -1)
   rownames(reshaped) = rep(rnames, each=3)
   return(reshaped)
 }
 
+mode <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
+
 get_consensus <- function(reshaped_table) {
   for (i in c(3,5,7)){ #cols for typer's allele 1
-    reshaped_table = cbind(reshaped_table, paste(reshaped_table[, i], reshaped_table[, i+1]))
-  
+    reshaped_table = cbind(reshaped_table, paste(reshaped_table[, i], reshaped_table[, (i+1)]))
   } 
-  
-  return(reshaped_table)
+  idx = (ncol(reshaped_table)-2):ncol(reshaped_table)
+  m = apply(reshaped_table[,idx], MARGIN=1, FUN=mode)
+  return(unlist(m))
 }
 
 
 reshaped <- reshape_results()
-consensus <- get_consensus(reshaped)
+reshaped = cbind(reshaped, get_consensus(reshaped))
